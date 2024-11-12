a30c9d292e85c842cb5c460dfec152fb045c3ea8
zowordya <56652979+zowordya@users.noreply.github.com>
2024-11-12 06:42:07 +0300

    Update pdf_parser.py

--- app/pdf_parser.py
+++ app/pdf_parser.py
@@ -1,4 +1,3 @@
-# pdf_parser.py
 import PyPDF2
 
 
@@ -8,15 +7,14 @@ def extract_text_from_pdf(file_path):
         reader = PyPDF2.PdfReader(file)
         text = ''
         for page in reader.pages:
-            text += page.extract_text() + '\n'  # Добавляем новую строку после каждой страницы
+            text += page.extract_text() + '\n' 
     return text
 
 
 def get_line_from_text(text, line_number):
     """Возвращает строку по указанному номеру."""
     lines = text.split('\n')
-    # Проверяем, существует ли строка с указанным номером
     if line_number <= len(lines):
-        return lines[line_number - 1]  # -1, потому что индексация начинается с 0
+        return lines[line_number - 1]
     else:
         return "Строка не существует."
